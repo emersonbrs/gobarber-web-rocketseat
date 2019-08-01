a0aa4d3865cc59a98bc9ff8183e370e690d054dc
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-08-01 18:58:17 -0300

    Configurando redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
@@ -6,14 +7,20 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+/* O store do redux obrigatoriamente deve vir após a importação do ReactotronConfig
+senão ele não vai ter acesso, por exemplo, as funções do sagaMonitor ou do createEnhancer */
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
